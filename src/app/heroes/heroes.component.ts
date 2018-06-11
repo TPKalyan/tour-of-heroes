17a5f19fd8f67301177acce97b4654e8fa0d2451
pavankt <pavankt@INpavankts-MacBook-Pro.local>
2018-06-11 21:00:31 +0530

    added router

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -21,8 +21,4 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 }
